51f9471a65ebc0fc19718377a946e0c23aa34033
Jon Palmisciano <jp@jonpalmisc.com>
2022-12-30 19:55:32 -0500

    Load settings more safely, add global settings

--- src/emake/core/settings.py
+++ src/emake/core/settings.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import tomli
 
 from typing import List, Optional
@@ -21,8 +23,8 @@ class Settings:
     build_copy_cc: bool
 
     def __init__(self, raw: SafeDict) -> None:
-        self.configure_args = raw["configure"]["args"] or []
-        self.configure_generator = raw["configure"]["generator"] or None
+        self.configure_args = raw["configure"].get("args") or []
+        self.configure_generator = raw["configure"].get("generator") or None
 
         self.build_jobs = raw["build"].get("jobs") or None
         self.build_copy_cc = raw["build"].get("copy_cc") or False
@@ -31,8 +33,14 @@ class Settings:
 def default() -> Settings:
     """Load user settings from the default path(s)."""
 
+    local_path = Path("emake.toml")
+    if local_path.is_file():
+        settings_path = local_path
+    else:
+        settings_path = Path.home().joinpath(".config", "emake.toml")
+
     try:
-        with open("emake.toml", "rb") as f:
+        with open(settings_path, "rb") as f:
             raw = tomli.load(f)
             raw = SafeDict(raw)
     except FileNotFoundError:
