0248af5d8428675fa67446e206c7ff765dcce84b
Keto <therealketo@gmail.com>
2023-03-10 22:12:40 -0500

    Use benedict for better settings parsing/handling (#6)

--- src/emake/core/settings.py
+++ src/emake/core/settings.py
@@ -5,50 +5,21 @@ try:
 except ModuleNotFoundError:
     import tomli as tomllib
 
+from benedict import benedict
 from typing import Dict, List, Optional
 
-# https://stackoverflow.com/a/3405143
-class SafeDict(dict):
-    """Dictionary which cannot produce key errors."""
 
-    def __missing__(self, key):
-        value = self[key] = type(self)()
-        return value
-
-
-class Settings:
-    """User settings structure."""
-
-    configure_args: List[str]
-    configure_generator: Optional[str]
-    configure_env: Dict[str, str]
-
-    build_jobs: Optional[int]
-    build_copy_cc: bool
-
-    def __init__(self, raw: SafeDict) -> None:
-        self.configure_args = raw["configure"].get("args") or []
-        self.configure_generator = raw["configure"].get("generator") or None
-        self.configure_env = raw["configure"].get("env") or {}
-
-        self.build_jobs = raw["build"].get("jobs") or None
-        self.build_copy_cc = raw["build"].get("copy_cc") or False
-
-
-def default() -> Settings:
+def default() -> benedict:
     """Load user settings from the default path(s)."""
 
-    local_path = Path("emake.toml")
-    if local_path.is_file():
-        settings_path = local_path
-    else:
+    settings_path = Path("emake.toml")
+    if not settings_path.is_file():
         settings_path = Path.home().joinpath(".config", "emake.toml")
 
     try:
         with open(settings_path, "rb") as f:
             raw = tomllib.load(f)
-            raw = SafeDict(raw)
     except FileNotFoundError:
-        raw = SafeDict()
+        raw = {}
 
-    return Settings(raw)
+    return benedict(raw)
